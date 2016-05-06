02d78e686b213412a8032450d796ab3326152c3c
shrgupta <shrddhgupta5@gmail.com>
2016-05-06 12:49:24 +0530

    Changes

--- Hello/Block/Main.php
+++ Hello/Block/Main.php
@@ -3,28 +3,46 @@ namespace Excellence\Hello\Block;
   
 class Main extends \Magento\Framework\View\Element\Template
 {   
+    protected $registry;
     protected $_testFactory;
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Excellence\Hello\Model\TestFactory $testFactory
+        \Excellence\Hello\Model\TestFactory $testFactory,
+        \Magento\Framework\Registry $registry
     )
     { 
         $this->testFactory=$testFactory;
+         $this->registry = $registry;
         parent::__construct($context);
     }
     protected function _prepareLayout()
     {
-
-          $data=$this->testFactory->create();
-          $collectionData = $data->getCollection()->setOrder('id', 'DESC');
+        $data=$this->testFactory->create();
+          $search = $this->registry->registry('search');
+          if(!empty($search)){
+           
+            $collectionData = $data->getCollection()
+                            ->addFieldToFilter(
+                                array('name', 'message','email'),
+                                array(
+                                    array('like'=>'%'.$search.'%'), 
+                                    array('like'=>'%'.$search.'%'),
+                                    array('like'=>'%'.$search.'%')
+                                )
+                            )
+                    ->setOrder('id', 'DESC');
+          }
+          else{
+            $collectionData = $data->getCollection()->setOrder('id', 'DESC');
+          }
+          if(isset($search)){
+            $this->setSearchTerm($search);
+          }
           
           $this->setCollectModel($collectionData);
 
     }
-    public function getSearchUrl($id)
-    {
-       return $this->_urlBuilder->getUrl("excellence/index/search/");
-    }
+  
     public function getDeleteUrl($id)
     {
        return $this->_urlBuilder->getUrl("excellence/index/delete/", array('id' => $id));
--- Hello/Controller/Hello/World.php
+++ Hello/Controller/Hello/World.php
@@ -4,17 +4,25 @@ namespace Excellence\Hello\Controller\Hello;
  
 class World extends \Magento\Framework\App\Action\Action
 {
+    protected $registry;
     protected $resultPageFactory;
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory)
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Registry $registry
+        )
     {
-        $this->resultPageFactory = $resultPageFactory;       
+        $this->resultPageFactory = $resultPageFactory;  
+        $this->registry = $registry;     
         return parent::__construct($context);
     }
      
     public function execute()
     {
+        $post = $this->getRequest()->getPost('data');
+        if(isset($post)){
+            $this->registry->register('search', $post['srch']);
+        }
         return $this->resultPageFactory->create(); 
     } 
 }
